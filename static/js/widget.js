74fd82b60a4d9e696017a615ac7aec6918ad4a6c
Besma M'charek <bmcharek@gmail.com>
2012-04-19 15:48:45 +0200

    final commit 1st version

--- static/js/widget.js
+++ static/js/widget.js
@@ -1,8 +1,8 @@
 /*here the js code comes that generates the widget */
 
 uhref = $("#jswidget").attr("href") ;
-ref   = "http://www.peerz.nl/people/greg"; //this would be somethin retrieved from json
-if( uhref ==  ref){
+ref   = "http://www.peerz.nl/people/greg"; //this would be something retrieved from json
+if( uhref ==  ref){//here nore checks should be made. eg: is the person authorized to place this pers. widget?
     $.ajax({
 	type : "GET",
 	url  : "/jsondata/",
@@ -14,7 +14,7 @@ if( uhref ==  ref){
             var pdom = $('<div id="outer"></div>');
 	    pdom.append(script);
             pdom.append('<h2>According to peerz</h2>');
-	    pdom.append('<img id="riasec_img" src="https://chart.googleapis.com/chart?cht=rs&amp;chs=330x330&amp;chxt=x&amp;chf=a,s,000000|bg,s,00000000&amp;chls=0&amp;chxr=0,0,105&amp;chxs=0,000000,10.5,-1,lt,8A8A8A&amp;chm=B,99d62a,0,1.0,5.0&amp;chd=t:64,0,37,0,113,12,64&amp;chxl=0:|Realistic|Investigative|Artistic|Social|Enterprising|Conventional">');
+	    pdom.append('<img id="riasec_img" src="'+ msg.result.chart.all + '">');
             pdom.append('<h3>' + msg.result.details.en.personal.personal +'</h3>'); 
             pdom.append('<div id="mywidget"></div>'); 
             $('#jswidget').append(pdom);
